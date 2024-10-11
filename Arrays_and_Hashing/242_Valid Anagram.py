5534208075d4d9dfa0cc491b21308c527048333c
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-10-11 14:57:44 -0700

    valid anagram

---
+++ Arrays_and_Hashing/242_Valid Anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        mapS, mapT = {}, {}
+
+        for i in range(len(s)):
+            mapS[s[i]] = mapS.get(s[i], 0) + 1
+            mapT[t[i]] = mapT.get(t[i], 0) + 1  
+        return mapS == mapT
