d8ad381c6b9be69ce03630e69616d3bfbb5b48db
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-10-11 21:58:42 -0700

    group anagram sort

--- Arrays_and_Hashing/49_Group_Anagrams.py
+++ Arrays_and_Hashing/49_Group_Anagrams.py
@@ -8,3 +8,13 @@ class Solution:
                 count[ord(c) - ord('a')] += 1
             result[tuple(count)].append(s)
         return result.values()
+    
+
+# sort function
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            result[sorted_s].append(s)
+        return list(result.values())
