e6ba1026305acaf578dccc5336c999759b537875
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-05 23:32:04 -0800

    valid palindrome 2

---
+++ Greedy_Algorithms/680_Valid_Palindrome_II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return self.palindrome(s, left + 1, right) or self.palindrome(s, left, right - 1)
+            left += 1
+            right -= 1
+        return True
+    def palindrome(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
