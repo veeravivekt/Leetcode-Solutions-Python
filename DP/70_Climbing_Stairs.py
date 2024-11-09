3abb22be75f53b782daaa9650cc459a4b99b8bdd
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-08 16:40:26 -0800

    climbing stairs top down

---
+++ DP/70_Climbing_Stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1] * n
+        def dfs(i):
+            if i >= n:
+                return i == n
+            if dp[i] != -1:
+                return dp[i]
+            dp[i] = dfs(i + 1) + dfs(i + 2)
+            return dp[i]
+        return dfs(0)
