2035c5dee4a0071d6093de04cddf66079f4c6d7e
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-08 18:38:24 -0800

    climbing stairs bottom up

--- DP/70_Climbing_Stairs.py
+++ DP/70_Climbing_Stairs.py
@@ -1,3 +1,4 @@
+# Top Down Approach
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [-1] * n
@@ -9,3 +10,14 @@ class Solution:
             dp[i] = dfs(i + 1) + dfs(i + 2)
             return dp[i]
         return dfs(0)
+    
+# Bottom up Approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
