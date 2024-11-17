ecfd817d3db0fb6e2aca8ad63a7dae4b32cfb91b
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-16 17:37:22 -0800

    Permutations Backtracking

--- Subsets/46_Permutations.py
+++ Subsets/46_Permutations.py
@@ -1,3 +1,4 @@
+# Subsets TC: O(n! * n) SC: O(n! * n)
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         permutations = [[]]
@@ -9,4 +10,17 @@ class Solution:
                     currentP.insert(i, n)
                     permutations.append(currentP)
         return permutations
-    
+
+# BackTracking TC: O(n! * n^2) SC: O(n! * n)
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        result = []
+        perms = self.permute(nums[1:])
+        for p in perms:
+            for i in range(len(p) + 1):
+                currP = p.copy()
+                currP.insert(i, nums[0])
+                result.append(currP)
+        return result
