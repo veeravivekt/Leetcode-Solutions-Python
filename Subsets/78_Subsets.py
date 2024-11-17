789bb5f0059514e82d8877de578924a0350aa8ae
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-16 16:32:16 -0800

    subsets backtracking

--- Subsets/78_Subsets.py
+++ Subsets/78_Subsets.py
@@ -9,3 +9,19 @@ class Solution:
                 currentSet.append(n)
                 subsets.append(currentSet)
         return subsets
+    
+# BackTracking
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                result.append(list(subset))
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+        dfs(0)
+        return result
