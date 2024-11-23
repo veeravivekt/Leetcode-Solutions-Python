f2d017ab4e60f18fe062c10cd85f8b3e4c9007ea
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-23 12:57:23 -0800

    update shortest word dist

--- Basics/243_Shortest_Word_Distance.py
+++ Basics/243_Shortest_Word_Distance.py
@@ -1,6 +1,11 @@
-# EC: Can word1 or word2 be null?, can wordDict be empty?, Max Lenth of words?, Max Length of wordDict?, word1 == word2?, casesensitive?, word1, word2 always exists in wordDict?
-# TC: O(n) -> checking each word at most once
-# SC: O(1) -> shortDist, position1, position2 (O(1))
+# EC: Can word1 or word2 be null?, can wordDict be empty?, Max Lenth of words?, Max Length of wordDict?, word1 == word2?, casesensitive?, word1, word2 always exists in wordDict? multiple occurances?
+"""
+word1 = "coding", word2 = "practice"
+["practice", "makes", "perfect", "coding", "makes"]
+s0: p1 = -1 | p2 = -1
+s1: p1 = -1 | p2 = 0
+s2: p1 = 3 | p2 = 0 | result = 3 - 0
+"""
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         # initialize a shortDist variable to keep track
@@ -17,3 +22,5 @@ class Solution:
             if position1 != -1 and position2 != -1:
                 shortDist = min(shortDist, abs(position1 - position2))
         return shortDist
+# TC: O(n) -> checking each word at most once
+# SC: O(1) -> shortDist, position1, position2 (O(1))
