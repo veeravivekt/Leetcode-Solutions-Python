f9adcec25249e472be67c186dc7749e77f07966c
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-22 18:42:42 -0800

    updated valid anagram

--- Basics/242_ValidAnagram.py
+++ Basics/242_ValidAnagram.py
@@ -1,12 +1,21 @@
+# TC: O(n) -> each character from each strings is checked atmost once
+# SC: O(1) -> charStore(O(26)), i(O(1))
+# Edge Cases: Empty Strings, Diff Lengths, case sensitive, size of strings, non Alpha
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Basecase: if lenths are diff return False
         if len(s) != len(t):
             return False
-        alpha = [0] * 26
+        # initialize a charStores
+        charStore = [0] * 26
+        # for each character in both s and t
         for i in range(len(s)):
-            alpha[ord(s[i]) - ord('a')] += 1
-            alpha[ord(t[i]) - ord('a')] -= 1
-        for n in alpha:
+            # add 1 to charStore for each char in s
+            charStore[ord(s[i]) - ord('a')] += 1
+            # subtract 1 to charStore for each char in s
+            charStore[ord(t[i]) - ord('a')] -= 1
+        # if everything is 0 in charStore return True
+        for n in charStore:
             if n != 0:
                 return False
         return True
