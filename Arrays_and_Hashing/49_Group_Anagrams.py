4cb31195ecb43f0fe70cd42e74b2a02544b06936
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-23 23:27:10 -0800

    update group ana

--- Arrays_and_Hashing/49_Group_Anagrams.py
+++ Arrays_and_Hashing/49_Group_Anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result = collections.defaultdict(list)
-
         for s in strs:
             count = [0] * 26
             for c in s:
@@ -10,12 +9,18 @@ class Solution:
         return result.values()
     
 
-# sort function
+# EC: empty str? all same? all unqiue? maxSize of strs? len of str? case-senstive?
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = defaultdict(list)
-        
+        # initalize a hashmap to store values
+        store = defaultdict(list)
+        # check every str
         for s in strs:
-            sorted_s = ''.join(sorted(s))
-            result[sorted_s].append(s)
-        return list(result.values())
+            # sort the string and check if it matches with any key
+            sortedS = ''.join(sorted(s))
+            # if yes then add it to the list
+            store[sortedS].append(s)
+        # return all values as list
+        return list(store.values())
+# TC: O(m * nlogn) -> m is len of str, sorting n numbers(O(nlogn))
+# SC: O(m * n) -> store(m len and n nums)
