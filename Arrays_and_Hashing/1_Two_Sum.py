9a2a53faef7366eac7aed05f899de82d2e6bf8f8
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-23 23:37:33 -0800

    update two sum

--- Arrays_and_Hashing/1_Two_Sum.py
+++ Arrays_and_Hashing/1_Two_Sum.py
@@ -1,8 +1,14 @@
+# EC: empty array?, target less than min sum?, multiple ans?
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
+        # have a hashmap to store passed elements
+        store = {}
+        # verify every element
         for i, n in enumerate(nums):
+            # check if target - num is present in hashmap
             diff = target - n
-            if diff in map:
-                return [map[diff], i]
-            map[n] = i
+            if diff in store:
+                return [store[diff], i]
+            store[n] = i
+# TC: O(n) -> each num is checked at most once
+# SC: O(n) -> store can have max of n unique nums
