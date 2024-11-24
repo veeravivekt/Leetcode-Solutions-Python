81bd1740a94f8cf4d758b2da0abb4f53adfcc97c
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-23 23:06:42 -0800

    update sqrt

--- Basics/69_Sqrt(x).py
+++ Basics/69_Sqrt(x).py
@@ -1,13 +1,24 @@
+# EC: range of x?, negative x?, perfect vs non perfect sqrs?, overflow?
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l, r = 0, x
-        while l <= r:
-            m = l + (r - l) // 2
-            n = m * m
-            if n > x:
-                r = m - 1
-            elif n < x:
-                l = m + 1
+        if x < 2:
+            return x
+        # initalize the range to search for sqrt
+        left, right = 2, x // 2
+        # search till left and right cross each other
+        while left <= right:
+            # caliculate mid value
+            mid = left + (right - left) // 2
+            square = mid * mid
+            # set left as mid + 1 if its less
+            if square < x:
+                left = mid + 1
+            # set right as mid - 1 if its more
+            elif square > x:
+                right = mid - 1
             else:
-                return m
-        return r
+                return mid   
+        # return right
+        return right
+# TC: O(logn) -> binary search each time search space is halfed
+# SC: O(1) -> left, right (O(1))
