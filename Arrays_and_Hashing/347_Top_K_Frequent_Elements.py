3d85ada932e508e2f0b9554467256914df9e73ac
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-24 19:24:03 -0800

    update k freq ele

--- Arrays_and_Hashing/347_Top_K_Frequent_Elements.py
+++ Arrays_and_Hashing/347_Top_K_Frequent_Elements.py
@@ -1,15 +1,20 @@
+# EC: same frequency?, maxLen of nums?
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-
+        # intialize a hashmap and count frequency of each nums
+        store = {}
         for n in nums:
-            count[n] = count.get(n, 0) + 1
-        for n, c in count.items():
-            freq[c].append(n)
-
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            res += freq[i]
-            if (len(res) == k):
-                return res
+            store[n] = store.get(n, 0) + 1
+        # add all elements to maxHeap
+        maxHeap = []
+        for key, value in store.items():
+            heappush(maxHeap, (-value, key))
+        # return top k elements
+        result = []
+        while k > 0:
+            value, key = heappop(maxHeap)
+            result.append(key)
+            k -= 1
+        return result
+# TC: O(nlogn) -> iternate every num for count(O(n)), heap(O(nlogn))
+# SC: O(n) -> worst case store, maxHeap stores n unqiue elements
