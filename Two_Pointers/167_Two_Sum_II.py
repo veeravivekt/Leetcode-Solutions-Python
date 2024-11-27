1667148aeae19494d8c23c86b0471ad6362a975e
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-11-26 16:52:25 -0800

    update two sum 2

--- Two_Pointers/167_Two_Sum_II.py
+++ Two_Pointers/167_Two_Sum_II.py
@@ -1,13 +1,28 @@
+# EC: empty arr?, -ve nums?, maxLen of arr?, num range?, multiple sums?, target less than min sum?
+"""
+[2,7,11,15]
+ l       r
+ 2 + 15? more
+ 2 + 11? more
+ 2 + 7 ? found
+"""
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(numbers) - 1
-        
-        while l < r:
-            sum = numbers[l] + numbers[r]
-            if sum == target:
-                return [l + 1, r + 1]
-            elif sum < target: 
-                l += 1
+        # initialize two pointers left and right
+        left, right = 0, len(numbers) - 1
+        # continue till left and right cross each other
+        while left < right:
+            total = numbers[left] + numbers[right]
+            # if sum is more move right pointer towards left
+            if total > target:
+                right -= 1 
+            # if sum is less move left pointer towards right
+            elif total < target:
+                left += 1
+            # if they are equal return result
             else:
-                r -= 1
+                return [left + 1, right + 1]
+        # return empty if not found anything
+        return []
+# TC: O(n) -> every element is checked at most once
+# SC: O(1) -> left, right pointers and total (O(1))
