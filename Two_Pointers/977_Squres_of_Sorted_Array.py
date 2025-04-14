c47b7e6b9a6276c19cc22aceb5257fe57d998cd2
veeravivekt <veeravivek.telagani@sjsu.edu>
2025-04-14 14:17:02 -0700

    untracked changes

--- Two_Pointers/977_Squres_of_Sorted_Array.py
+++ Two_Pointers/977_Squres_of_Sorted_Array.py
@@ -1,17 +1,32 @@
+# EC: empty, single element, All -ve or +ve nums, array with 0, array with only 0's, duplicate nums, Array with max or min values
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        l, r = 0, n - 1
-        highestSquare = n - 1
-        squares = [0 for i in range(n)]
-        while l <= r:
-            lSquare = nums[l] ** 2
-            rSquare = nums[r] ** 2
-            if lSquare > rSquare:
-                squares[highestSquare] = lSquare
-                l += 1
+        """
+        [-4,-1,0,3,10]
+            ^    ^
+        -4 or 10 -> 10
+        -4 or 3 -> -4
+        -1 or 3 -> 3
+        -1 or 0 -> -1
+        0 -> 0
+        """
+        # initialzie a result array and pos
+        sortedArr = [0] * len(nums)
+        pos = len(nums) - 1
+        left, right = 0, len(nums) - 1
+        # run a while loop until left > right
+        while left <= right:
+            # check which abs value is greater between left and right pointers
+            # if left is greater then assign its square to pos in array
+            if abs(nums[left]) > abs(nums[right]):
+                sortedArr[pos] = nums[left] ** 2
+                left += 1
+            # if right is greater do the same
             else:
-                squares[highestSquare] = rSquare
-                r -= 1
-            highestSquare -= 1
-        return squares
+                sortedArr[pos] = nums[right] ** 2
+                right -= 1
+            pos -= 1
+        # return array
+        return sortedArr
+# TC: O(n) -> each number is being iterated once
+# SC: O(n) -> sortedArr -> O(n), pos|left|right -> O(1)
